d1ce4e59fa7c852be72de705b23d727aaa0a1b65
VIMER <leon.2080emba3465@gmail.com>
2025-07-11 03:30:18 +0200

    main

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  token: '7967520625:AAEGlib_WG86qUPC0fWyVcyKrRTydOaP8rs',
+  adminId: 8172633389, // твой Telegram ID для админ-панели
+  rates: {
+    EUR_RUB: 90,       // 1 EUR = 90 RUB
+    RUB_EUR: 1/90,
+    BTC_USD: 27000,
+    ETH_USD: 1800,
+    USDT_USD: 1,
+    TRX_USD: 0.065,
+    // Для упрощения курсы между криптами считать через USD
+  },
+};
